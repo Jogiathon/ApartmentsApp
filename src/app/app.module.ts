edb9b05124cd0855d4206ef45729acd61222dcf3
Jorge Sanchez <jorgesanchez@qualitestgroup.com>
2021-02-06 06:20:26 -0800

    Added the nav and property components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+   ],
   imports: [
     BrowserModule
   ],
---
+++ src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-card',
+  template: `<h1>I am a card</h1>`,
+  styleUrls: ['./property-card.component.css']
+})
+export class PropertyCardComponent {
+
+}
